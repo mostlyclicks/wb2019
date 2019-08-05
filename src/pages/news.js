ca324c13f96d747d070f6a48214a920f21c8fb55
mostlyclicks <carlos@mostlyclicks.com>
2019-08-05 06:53:15 -0600

    added news index starter

---
+++ src/pages/news.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+const NewsIndex = () => (
+  <Layout>
+    <SEO title="News" />
+    <h1>News Index Page</h1>
+    <p>Overview</p>
+    <p>News with keyword links back to projects and service</p>
+  </Layout>
+)
+
+export default NewsIndex
