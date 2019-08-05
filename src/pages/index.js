e8b389267c51a49ac856dbfcda135c7f1fc1e725
mostlyclicks <carlos@mostlyclicks.com>
2019-08-05 06:40:31 -0600

    removed gatsby image - home h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Staging</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Wieser Brother's H1</h1>
+    <p>WB intro copy</p>
+    <p>Call to action</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+     
     </div>
     <Link to="/page-2/">Go to page 2</Link><br/>
     <Link to="/projects/">Go to projects</Link>
