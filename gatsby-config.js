91321f0ab76090eae382ef3aeb2ed33d3c76f85c
mostlyclicks <carlos@mostlyclicks.com>
2019-08-16 06:49:10 -0600

    add employment ops index, add contentful plugin, test contentful connection

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `6iwqaagnp0yo`,
+        accessToken: `B7pU2_aB4CM_ABdL5xR0K65uMNzWCHCJL-Gls36sm0s`
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/pages/employment-opportunities.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { graphql } from 'gatsby'
+import { Link } from 'gatsby'
+
+// const EmpOpsPage = ({ data: { allContentfulEmploymentOpportunities } }) =>
+
+const EmpOpsPage = ({data: { allContentfulEmploymentOpportunities }}) => (
+  <div>
+    <h1>Hello2 </h1>
+    <ul>
+    
+        {allContentfulEmploymentOpportunities.edges.map(({ node }) => (
+          <li><Link to={"employment-opportunities/" + node.slug}>{node.title}</Link></li>
+        
+        ))}
+    
+    </ul>
+  </div>
+)
+
+export const query = graphql`
+{
+  allContentfulEmploymentOpportunities {
+    edges {
+      node {
+        title
+        slug
+      }
+    }
+  }
+}
+`
+
+export default EmpOpsPage
