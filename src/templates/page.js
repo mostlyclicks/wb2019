762e872e2bf6b35fcf37de7a0dcd4a662ad6f972
mostlyclicks <carlos@mostlyclicks.com>
2019-11-19 07:56:17 -0700

    disabled page template to deploy

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 // import { graphql } from 'gatsby'
-import Layout from '../components/layout'
+// import Layout from '../components/layout'
 
-function Page({ data }) {
-  return (
-    <Layout>
-      <h2>Contentful Integration in Progress</h2>
-    </Layout>
-  )
-}
+// function Page({ data }) {
+//   return (
+//     <Layout>
+//       <h2>Contentful Integration in Progress</h2>
+//     </Layout>
+//   )
+// }
